fdf45d95e8363fe3426a93ff8ba79a951177341e
Diksha <diksha@macbookpro>
2025-12-09 13:07:43 -0800

    starting work, flight file copied

---
+++ main.cpp
@@ -0,0 +1,17 @@
+// COMSC-210 | Final Pt.1 | Diksha Tara Natesan
+// IDE used: Vim/Terminal
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main(){
+
+
+
+
+
+}
+
