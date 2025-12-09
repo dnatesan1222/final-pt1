57922be434720b4479e80959590e77c2c484644c
Diksha <diksha@macbookpro>
2025-12-09 13:18:11 -0800

    implementing m1

--- main.cpp
+++ main.cpp
@@ -8,9 +8,25 @@
 using namespace std;
 
 int main(){
+	//Milestone 1
+	map<string, int> airports;
+	
+	ifstream fin("routes.txt")
+	if (!fin){
+		cout << "Error: The file "routes.txt" could not be accessed." << endl;
+		return 1;
+	}
 
+	string orig;
+	string dest;
+	while (fin >> orig >> dest){
+		airports[orig] += 1;		//come back to check/test this logic
+		airports[dest] += 1;
+	}
 
-
+	//print to test?
+	cout << "All airport traffic counts:" << endl;
+	for (auto it = airports.begin(); it != airports.end(); it++)
 
 
 }
